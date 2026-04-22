249640f1882a43101923f5ed8f29c090f70272bf
dshkchv <125150338+dshkchv@users.noreply.github.com>
2026-04-22 22:15:00 +0300

    Delete Trading-Test-news_analysis directory

--- Trading-Test-news_analysis/frontend/src/config.ts
+++
@@ -1,5 +0,0 @@
-export const DATALOADER_URL = import.meta.env.VITE_DATALOADER_URL || 'http://localhost:5000';
-export const TESTING_URL = import.meta.env.VITE_TESTING_URL || 'http://localhost:5001';
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://db-service:8000';
-export const NEWS_URL = 'http://localhost:8002';
-export const TICKERLIST = ["SBER", "VTBR", "LKOH", "T", "VKCO", "YDEX", "GAZP"]
